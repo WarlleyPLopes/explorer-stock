bbc932cd929b9b86099d72a86cd68c89ae48f3d8
Warlley Lopes <warlleylopes123@gmail.com>
2024-05-14 20:12:26 +0100

    versioning code

---
+++ src/routes/index.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter } from "react-router-dom";
+
+import { useAuth } from "../hooks/auth";
+import { USER_ROLE } from "./utils/roles";
+
+import { AuthRoutes } from "./auth.routes";
+import { SaleRoutes } from "./sale.routes";
+import { AdminRoutes } from "./admin.routes";
+import { CustomerRoutes } from "./customer.routes";
+
+export function Routes() {
+  const { user } = useAuth();
+
+  function AccessRoutes() {
+    switch (user.role) {
+      case USER_ROLE.ADMIN:
+        return <AdminRoutes />;
+      case USER_ROLE.CUSTOMER:
+        return <CustomerRoutes />;
+      case USER_ROLE.SALE:
+        return <SaleRoutes />;
+      default:
+        return <CustomerRoutes />;
+    }
+  }
+
+  return (
+    <BrowserRouter>
+      {user ? <AccessRoutes /> : <AuthRoutes />}
+    </BrowserRouter>
+  );
+}
